49c0d40e6f4757806a593baf36d05b88b387ca30
IriKa <qiujie.jq@gmail.com>
2018-05-01 20:34:26 +0800

    Changed cache dir from /tmp to /data

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 # ccache
-export CCACHE_DIR=/tmp/ccache
+export CCACHE_DIR=/data/.ccache
 export USE_CCACHE=1
 if [ ! -z "$GIT_NAME" ] && [ ! -z "$GIT_EMAIL" ]; then
     git config --global user.name $GIT_NAME
