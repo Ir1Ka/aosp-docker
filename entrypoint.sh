dec53280263110ed1a2e30064d16be010677a31c
IriKa <qiujie.jq@gmail.com>
2018-06-28 14:52:59 +0800

    Fix issue withc spaces in environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 export CCACHE_DIR=/data/.ccache
 export USE_CCACHE=1
 if [ ! -z "$GIT_NAME" ] && [ ! -z "$GIT_EMAIL" ]; then
-    git config --global user.name $GIT_NAME
-    git config --global user.email $GIT_EMAIL
+    git config --global user.name "$GIT_NAME"
+    git config --global user.email "$GIT_EMAIL"
 else
     echo "GIT_NAME or GIT_EMAIL do not set."
 fi
