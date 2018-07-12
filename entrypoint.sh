0c113643d809c0617211a2b0a70a43942358a19b
IriKa <qiujie.jq@gmail.com>
2018-07-12 14:52:15 +0800

    Remove the git config command
    You can use the user initial script to config it.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,6 @@ set -e
 # ccache
 export CCACHE_DIR=/data/.ccache
 export USE_CCACHE=1
-if [ ! -z "$GIT_NAME" ] && [ ! -z "$GIT_EMAIL" ]; then
-    git config --global user.name "$GIT_NAME"
-    git config --global user.email "$GIT_EMAIL"
-else
-    echo "GIT_NAME or GIT_EMAIL do not set."
-fi
 
 # Exec the init script if it exist.
 if [ -x "/data/$INIT_SCRIPT" ]; then
